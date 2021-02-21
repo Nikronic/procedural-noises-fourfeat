b4f4b48e97bc8ce62caaf70eb68695295fd14295
Mohammad Doosti Lakhani <mdoosti@wks-14-53.mpi-inf.mpg.de>
2021-02-21 16:11:55 +0100

    add exp for long low ratio varatioal scale #1

--- slurm_jobs/train_fourfeat_gpu.sh
+++ slurm_jobs/train_fourfeat_gpu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
 #SBATCH -p gpu20
-#SBATCH -t 1-23:00:00
-#SBATCH -J Perlin
+#SBATCH -t 02:00:00
+#SBATCH -J OctaveSimplexPerlin
 #SBATCH -D /HPS/deep_topopt/work/procedural-noises-fourfeat
 #SBATCH -o /HPS/deep_topopt/work/procedural-noises-fourfeat/logs/slurm/fourfeat/slurm-%x-%j.log
 #SBATCH --gres gpu:1
